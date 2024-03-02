1dcd83116caabe0443d8c0ad2eac6e59191a7c98
User <gabrielkleber006@gmail.com>
2024-03-02 12:04:16 -0300

    Exercicio de fixação: Classes e métodos implementados simulando um carro

---
+++ src/Car.java
@@ -0,0 +1,23 @@
+public class Car {
+    int velocidadeAtual = 0;
+
+    void aceleraAe() {
+        if (velocidadeAtual >= 300) {
+            System.out.println("Vai com calma calabreso, tá muito rápido.. velocidade:" + velocidadeAtual);
+        }
+        velocidadeAtual += 15;
+        System.out.println("Eu sou a velocidade!!! acelerando:" + velocidadeAtual + "km/h");
+    }
+
+    void freiaAe() {
+        if (velocidadeAtual == 0) {
+            System.out.println("O carro tá parando anta!!, acelera nessa p*&#... Velocidade:" + velocidadeAtual + "km/h");
+        }
+        velocidadeAtual -= 5;
+        System.out.println("FREIA, FREIA VAI BATER CARAIO!! FREIANDO:" + velocidadeAtual + "km/h");
+    }
+
+    void mostraAvelocidadeAe() {
+        System.out.println("Sua velocidade é atual é de:" + velocidadeAtual + "km/h");
+    }
+}
